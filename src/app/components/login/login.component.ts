ee4640fd158315dc0d88087aaea4b621a3f4d5a5
Marcklen <marcklen@icloud.com>
2022-12-12 23:05:47 -0300

    validacao de email e senha e botao de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login: Login = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validarCampos(): boolean {
+    if(this.email.valid && this.senha.valid) {
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
